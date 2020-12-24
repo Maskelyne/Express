ff54099280770cb05061ae6af82932965f80ff78
Artem <Matusevich>
2020-12-24 16:32:20 +0300

    Разметка формы

--- source/js/modules/forms.js
+++ source/js/modules/forms.js
@@ -12,4 +12,44 @@
 
   btnRevers.addEventListener('click', formRevers);
 
+  $("#point-1").keyup(function () {
+    if ($(this).val()) {
+      $(this).addClass("not-empty");
+    } else {
+      $(this).removeClass("not-empty");
+    }
+  });
+
+  $("#point-2").keyup(function () {
+    if ($(this).val()) {
+      $(this).addClass("not-empty");
+    } else {
+      $(this).removeClass("not-empty");
+    }
+  });
+
+  $("#point-3").keyup(function () {
+    if ($(this).val()) {
+      $(this).addClass("not-empty");
+    } else {
+      $(this).removeClass("not-empty");
+    }
+  });
+
+  $("#weight").keyup(function () {
+    if ($(this).val()) {
+      $(this).addClass("not-empty");
+    } else {
+      $(this).removeClass("not-empty");
+    }
+  });
+
+  $("#volume").keyup(function () {
+    if ($(this).val()) {
+      $(this).addClass("not-empty");
+    } else {
+      $(this).removeClass("not-empty");
+    }
+  });
+
 })();
