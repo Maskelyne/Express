5891407bf40f4c3277a8290e84a4037f04cb7782
Artem Matusevich <artiom_filatov@list.ru>
2020-12-27 18:38:38 +0300

    Разметка слайдера отзывов

--- source/js/modules/forms.js
+++ source/js/modules/forms.js
@@ -2,54 +2,72 @@
 
 (function () {
 
-  var btnRevers = document.querySelector('.button-revers');
+  var formOrder = document.querySelector('.form-order__box');
 
-  var formRevers = function (evt) {
-    evt.preventDefault();
+  if (formOrder) {
 
-    btnRevers.classList.toggle('button-revers--active');
-  };
+    var btnRevers = formOrder.querySelector('.button-revers');
+    var number = 0;
 
-  btnRevers.addEventListener('click', formRevers);
+    var formReverse = function () {
+      number = number + 180;
+      btnRevers.style.transform = 'rotate(' + number + 'deg)';
+    };
 
-  $("#point-1").keyup(function () {
-    if ($(this).val()) {
-      $(this).addClass("not-empty");
-    } else {
-      $(this).removeClass("not-empty");
-    }
-  });
+    btnRevers.addEventListener('click', formReverse);
 
-  $("#point-2").keyup(function () {
-    if ($(this).val()) {
-      $(this).addClass("not-empty");
-    } else {
-      $(this).removeClass("not-empty");
-    }
-  });
+    $("#point-1").keyup(function () {
+      if ($(this).val()) {
+        $(this).addClass("not-empty");
+      } else {
+        $(this).removeClass("not-empty");
+      }
+    });
 
-  $("#point-3").keyup(function () {
-    if ($(this).val()) {
-      $(this).addClass("not-empty");
-    } else {
-      $(this).removeClass("not-empty");
-    }
-  });
+    $("#point-2").keyup(function () {
+      if ($(this).val()) {
+        $(this).addClass("not-empty");
+      } else {
+        $(this).removeClass("not-empty");
+      }
+    });
 
-  $("#weight").keyup(function () {
-    if ($(this).val()) {
-      $(this).addClass("not-empty");
-    } else {
-      $(this).removeClass("not-empty");
-    }
-  });
+    $("#point-3").keyup(function () {
+      if ($(this).val()) {
+        $(this).addClass("not-empty");
+      } else {
+        $(this).removeClass("not-empty");
+      }
+    });
 
-  $("#volume").keyup(function () {
-    if ($(this).val()) {
-      $(this).addClass("not-empty");
-    } else {
-      $(this).removeClass("not-empty");
-    }
-  });
+    $("#weight").keyup(function () {
+      if ($(this).val()) {
+        $(this).addClass("not-empty");
+      } else {
+        $(this).removeClass("not-empty");
+      }
+    });
+
+    $("#volume").keyup(function () {
+      if ($(this).val()) {
+        $(this).addClass("not-empty");
+      } else {
+        $(this).removeClass("not-empty");
+      }
+    });
+
+    var inputmask_options;
+
+    inputmask_options = {
+      mask: "99/99/9999",
+      alias: "date",
+      showMaskOnHover: false,
+      showMaskOnFocus: true,
+    };
+
+    $("#point-3").inputmask("99/99/9999", inputmask_options);
+    $("#point-3").datepicker();
+
+  }
 
 })();
