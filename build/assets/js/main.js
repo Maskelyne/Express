78cb6cba5bba29138396f76c1fdc6ce6fa1ccfa3
Artem <Matusevich>
2021-03-15 17:36:39 +0300

    Правки

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1572,6 +1572,8 @@
   var timeTable = document.querySelector('.timetable');
   var timeTableText = document.querySelectorAll('.timetable__text p');
   var timeTableBox = document.querySelectorAll('.timetable__inner table');
+  var timeTableText2 = document.querySelectorAll('.timetable__text-2 p');
+  var timeTableBox2 = document.querySelectorAll('.timetable__inner-mobile table');
 
   if (timeTable) {
     var removeActive = function (array, activeClass) {
@@ -1591,6 +1593,28 @@
         removeActive(timeTableBox, 'active-table');
         timeTableText[i].classList.add('active-tabs');
         timeTableBox[i].classList.add('active-table');
+      });
+    });
+  }
+
+  if (timeTableText2) {
+    var removeActive = function (array, activeClass) {
+      array.forEach(function (item) {
+        item.classList.remove(activeClass);
+      });
+      return;
+    };
+
+    timeTableText2.forEach(function (item, i) {
+      timeTableText2[0].classList.add('active-tabs');
+      timeTableBox2[0].classList.add('active-table');
+      item.addEventListener('click', function (evt) {
+        evt.preventDefault();
+
+        removeActive(timeTableText2, 'active-tabs');
+        removeActive(timeTableBox2, 'active-table');
+        timeTableText2[i].classList.add('active-tabs');
+        timeTableBox2[i].classList.add('active-table');
       });
     });
   }
